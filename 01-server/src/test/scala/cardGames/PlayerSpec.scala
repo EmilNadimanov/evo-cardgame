0d7ee877f398d47446e74cb1740243e5983b1742
EmilNadimanov <nadimaemi@gmail.com>
2022-07-13 18:25:26 +0200

    fixed player test

--- 01-server/src/test/scala/cardGames/PlayerSpec.scala
+++ 01-server/src/test/scala/cardGames/PlayerSpec.scala
@@ -1,31 +1,39 @@
 package evo.cardgame
 package cardGames
 
+import cardGames.messages.PlayerMessage
 import common.cards.card.CardRankBased
-import common.utils.SyncIOSuite
+import common.utils.IOSuite
 
-import cats.effect.IO
+import cats.effect.{IO, _}
+import fs2.concurrent.Queue
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-class PlayerSpec extends AnyWordSpec with Matchers with SyncIOSuite {
+class PlayerSpec extends AnyWordSpec with Matchers with IOSuite {
 
   import PlayerSpec._
 
   "changing points" should {
     "work as expected" in ioTest {
       for {
-        player99 <- thePlayer
-        player77 <- player99.takePoints(22)
+        player99  <- thePlayer
+        player77  <- player99.takePoints(22)
         player100 <- player77.givePoints(23)
-      } yield assert {
-        player99.points == 99 && player77.points == 77 && player100.points == 100
-      }
+      } yield
+        assert {
+          player99.score == 99 && player77.score == 77 && player100.score == 100
+        }
     }
   }
 
 }
 
 object PlayerSpec {
-  val thePlayer: IO[Player[IO, CardRankBased]] = Player(42, "Bobby Fischer", 99)
+  def thePlayer(implicit c: Concurrent[IO]): IO[Player[IO, CardRankBased]] =
+    for {
+      queue  <- Queue.circularBuffer[IO, PlayerMessage](1)
+      player <- Player[IO, CardRankBased]("Bobby Fischer", 99, queue)
+    } yield player
+
 }
